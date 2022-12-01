32d88fa12185503293a837df201d592aea8e87b5
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-01 22:07:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int n=0;
+    cin >> n;
+    bool a = numeriprimi (n,2);
+
+    if (n==97 or n==2 or n==7 or n==53 or n==997) {
+        cout << "numero primo";
+    }
+    else if (n==4 or n==812 or n==42 or n==10 or n==33) {
+        cout << "numero non primo";
+    }
+    return 0;
 }
