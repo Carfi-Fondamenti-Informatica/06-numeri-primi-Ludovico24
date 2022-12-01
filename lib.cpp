971c254a785925313077013113747a47edfa8ed1
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-01 22:06:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,9 @@
-
+boolnumeriprimi(int n,inti) {
+    for (i = 2; i < n; i++) {
+        se(n % i ==0) {
+            return false;
+        } altro {
+            restituiscitrue;
+        }
+    }
+}
