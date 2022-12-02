794413f81b235736b8c8858f48d2618d35a7f986
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-02 18:15:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,15 @@
-#include <iostream>
+#include<iostream>
 #include "lib.h"
-
 using namespace std;
 
 int main() {
-    int n;
-    int i;
-    bool ris;
-    cin>>n;
-    i=n-1;
-    ris=num_prim(n,i);
-    if (ris==1)
-    {
-        cout<<"numero primo";
+    int p;
+    cin >> p;
+    if (numeroprimo(p) == 0) {
+        cout << "numero primo" << endl;
     }
     else {
-        cout<<"numero non primo";
+        cout << "numero non primo" << endl;
     }
     return 0;
-}
+}    
