b1852ba2c3dddd947dbc8f390287e0adbb6c950f
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-02 17:57:30 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,11 @@
-boolnumeriprimi(int n,inti) {
-    for (i = 2; i < n; i++) {
-        se(n % i ==0) {
-            return false;
-        } altro {
-            restituiscitrue;
-        }
-    }
+boolnum_prim(int n,inti)
+{
+    int r;
+    se(i <2)
+        restituiscitrue;
+    r = n % i;
+    se(r ==0)
+        return false;
+    altro
+        r = num_prim(n, i - 1);
 }
